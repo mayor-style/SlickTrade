0144179884810da3f1f6436ea643b282e48111be
slick Outlaw <teslimshehu17@gmail.com>
2024-12-29 14:41:54 +0100

    finished up the top nav and the sidebar for now

--- src/components/UserDashboard/Sidebar.jsx
+++ src/components/UserDashboard/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSearch, FaBell, FaQuestionCircle, FaUserCircle } from 'react-icons/fa';
 import { Link, Outlet } from 'react-router-dom';
 import { FaWallet, FaCogs } from 'react-icons/fa';
@@ -6,9 +6,15 @@ import { MdOutlineSwapHoriz, MdOutlineHelpCenter } from 'react-icons/md';
 import { HiOutlineUsers } from 'react-icons/hi';
 import { RiDashboardLine } from 'react-icons/ri';
 import { BsChatDots } from 'react-icons/bs';
+import { RiMenuLine } from "react-icons/ri";
+import { IoMdClose } from "react-icons/io";
 import logo from '../../assets/imgs/logo.png';
 
 const Sidebar = () => {
+ 
+  const [isSearch, setIsSearch] =useState(false);
+  const [isOpen, setIsopen] = useState(false);
+
   const pages = [
     { name: "Dashboard", path: "/user/dashboard", icon: <RiDashboardLine /> },
     { name: "Transactions", path: "/user/transactions", icon: <MdOutlineSwapHoriz /> },
@@ -16,19 +22,29 @@ const Sidebar = () => {
     { name: "Vendors", path: "/user/vendors", icon: <HiOutlineUsers /> },
     { name: "Messages/Chat", path: "/user/messages", icon: <BsChatDots /> },
     { name: "Profile Settings", path: "/user/profile-settings", icon: <FaCogs /> },
-    { name: "Support/Help", path: "/user/support", icon: <MdOutlineHelpCenter /> },
+    { name: "Support/Help", path: "/user/support", icon: <FaQuestionCircle /> },
+    ...(isOpen
+        ? [{ name: "Notifications", path: "/user/notifications", icon: <FaBell /> }]
+        : []),
   ];
 
-  return (
-    <div className="h-full bg-hero bg-cover bg-center relative flex ">
-    
-        <div className="absolute h-full w-full inset-0 backdrop-blur-sm"></div>
-          {/* Sidebar */}
-          
-      <div className="h-full fixed z-10 top-0 left-0 flex flex-col max-md:hidden  border  backdrop-blur-md border-t-gray border-l-gray border-b-gray border-r-gray overflow-auto scrollbar-thin scrollbar-thumb-dark-gray scrollbar-track-gray lg:max-w-[270px] max-lg:w-[110px] py-4 px-3 pb-10 transition-all duration-300">
 
+  return (
+    <div className="h-full bg-hero bg-cover bg-center  relative flex ">
+        {/*OVERLAY */}
+        <div className={`absolute h-full w-full inset-0  ${isOpen? 'max-md:z-10 backdrop-blur-lg'  : ''} backdrop-blur-sm`}></div>
+
+            {/* BEGINNING OF SIDEBAR 😎😎 */}       
+      <div className={`h-full transition-all ease-in-out duration-300 fixed z-10 top-0 left-0 flex flex-col ${isOpen ? 'max-xs:w-full max-md:flex transform origin-left z-20 max-md:w-[270px] bg-black bg-opacity-70 ':' transform origin-left max-md:hidden '}  border  backdrop-blur-md border-t-gray border-l-gray border-b-gray border-r-gray overflow-auto scrollbar-thin scrollbar-thumb-dark-gray scrollbar-track-gray lg:max-w-[270px] max-lg:w-[110px] py-4 px-3 pb-10 `}>
+
+         {/*HAMBURGER MENU */}
+         <div className="cursor-pointer w-fit md:hidden text-white transition-all ease-in-out hover:bg-gold hover:text-black duration-300 ml-5 text-center flex md:hdden text-3xl bg-glass rounded-full border border-dark-gray  p-2 items-center justify-center mb-7"
+          onClick={()=>setIsopen(!isOpen)}
+          >
+             <IoMdClose className=''/>
+          </div>
         {/* Logo */}
-        <div className="flex justify-center lg:px-6 border-b border-b-gray pb-4 items-center gap-1 mb-10">
+        <div className={`flex justify-center ${isOpen? 'max-md:hidden': ""} lg:px-6 border-b  border-b-gray pb-4 items-center gap-1 mb-10`}>
           <img src={logo} alt="SlickTrade Logo" className="object-cover w-11 h-auto" />
           <h1 className="tracking-wide text-xl max-lg:hidden text-white font-bold">
             <span className="text-gold">Slick</span>Trade
@@ -36,17 +52,18 @@ const Sidebar = () => {
         </div>
 
         {/* Menu Section */}
-        <ul className="flex w-full flex-col gap-4 px-4">
+        <ul className="flex w-full transition-all duration-300 ease-in-out flex-col gap-4 px-4">
           {pages.map((page) => (
             <li key={page.name} className='group'>
               <Link
-                className="flex items-center gap-3 p-2 lg:group-hover:bg-gold max-lg:justify-center group-hover:text-black text-white rounded-full transition-all duration-300"
+                className={`flex items-center gap-3 p-2 lg:group-hover:bg-gold max-lg:justify-center group-hover:text-black text-white rounded-full transition-all ease-in-out duration-300  ${isOpen? 'max-md:group-hover:bg-gold max-md:justify-start' :  ''}`}
                 to={page.path}
+                onClick={()=>setIsopen(!isOpen)}
               >
-                <span className="'text-gold lg:group-hover:bg-black  max-lg:hover:text-black  max-lg:hover:bg-gold bg-gray max-lg:p-3 p-2 rounded-full max-lg:text-lg transition-all duration-300">
+                <span className={`text-gold lg:group-hover:bg-black  max-lg:hover:text-black ${isOpen? 'max-md:group-hover:bg-black max-md:hover:text-gold' :  ''}  max-lg:hover:bg-gold bg-gray max-lg:p-3 p-2 rounded-full max-lg:text-lg transition-all duration-300`}>
                   {page.icon}
                 </span>
-                <span className="text-white group-hover:text-black group-hover:font-semibold group-hover:text-sm max-lg:hidden">
+                <span className={`text-white group-hover:text-black group-hover:font-semibold ${isOpen ? 'max-md:block': 'max-md:hidden'} group-hover:text-sm max-lg:hidden`}>
                   {page.name}
                 </span>
               </Link>
@@ -54,14 +71,40 @@ const Sidebar = () => {
           ))}
         </ul>
       </div>
+            {/* END OF SIDEBAR 😎😎 */}
 
-      {/* Main Content Area */}
-      <div className="max-md:ml-0 flex-1 relative right-0 flex-col z-1 h-full overflow-auto max-md:w-full user-right-dashboard" >
-        
+            {/* MAIN CONTENT AREA 😎😎 */}
+      <div className="max-md:ml-0 flex-1 relative right-0 flex-col z-1 h-full overflow-auto max-md:w-full user-right-dashboard" >    
         {/* Top Navbar */}
-        <div className="flex px-4 justify-between h-[67px] sm:h-[77px] text-white border-b border-b-gray items-center">
+        <div className="flex px-4 justify-between h-[60px] md:h-[77px] text-white border-b border-b-gray items-center">
+
+          {/*TOP NAVBAR CONTENT FOR SCREEN BELOW MEDIUM SIZE DOWNWARD!!! */}
+          {/*HAMBURGER MENU */}
+          <div className="cursor-pointer transition-all ease-in-out duration-300 md:hidden text-2xl"
+          onClick={()=>setIsopen(!isOpen)}
+          >
+             <RiMenuLine/>
+          </div>
+
+          {/* Logo */}
+          <div className="flex md:hidden justify-center items-center gap-1 ">
+          <img src={logo} alt="SlickTrade Logo" className="object-cover w-8 h-auto" />
+          <h1 className="tracking-wide text-md  text-white font-bold">
+            <span className="text-gold">Slick</span>Trade
+          </h1>
+        </div>
+
+        {/* Search Input */}
+        <div className={`search-bar md:hidden border py-1 text-sm bg-gray px-3 transition-all ease-in-out duration-300 rounded-full justify-between flex items-center ${isSearch? 'border-dark-gray': 'border-none bg-transparent' }`} >
+            <input type="text" className={`outline-none ${ isSearch? '':'hidden' } bg-transparent`} placeholder="Search here..." />
+            <button type="submit" onClick={()=>setIsSearch(!isSearch)} className={`${isSearch? '':'text-lg'} font-medium`}>
+              <FaSearch />
+            </button>
+          </div>
+
+          {/* TOP NAVBAR CONTENTS FOR SCREEN ABOVE MEDIUM  SIZE!!! */}
           {/* Search Input */}
-          <div className="search-bar border py-1 max-sm:text-sm bg-gray px-3 rounded-full justify-between flex items-center  border-dark-gray">
+          <div className="search-bar max-md:hidden border py-1 max-sm:text-sm bg-gray px-3 rounded-full justify-between flex items-center  border-dark-gray">
             <input type="text" className="outline-none sm:w-[200px] lg:w-[300px] bg-transparent" placeholder="Search here..." />
             <button type="submit" className="font-medium">
               <FaSearch />
@@ -69,7 +112,7 @@ const Sidebar = () => {
           </div>
 
           {/* Icons */}
-          <div className="admin-nav-right sm:text-xl flex items-center gap-2 sm:gap-4">
+          <div className="admin-nav-right max-md:hidden sm:text-xl flex items-center gap-2 sm:gap-4">
             <Link
               to="#notifications"
               className="nav-icon bg-gray p-2 text-gold rounded-full hover:bg-gold hover:text-black transition-all ease-in-out duration-300"
@@ -97,6 +140,7 @@ const Sidebar = () => {
         </div>
 
       </div>
+            {/*END OF MAIN CONTENT AREA 😎😎 */}
     </div>
   );
 };
--- src/pages/UserDashboardPages/Dashboard.jsx
+++ src/pages/UserDashboardPages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='bg-glass border-dark-gray border relative rounded-lg py-[100px] justify-center items-center text-center flex m-auto sm:px-20 '>
+    <div className='bg-glass min-h-screen border-dark-gray border relative rounded-lg py-[100px] justify-center items-center text-center flex m-auto sm:px-20 '>
       <h1 className='lg:text-6xl sm:text-4xl text-3xl m-auto text-white text-center font-bold'>
         Welcome To Your Dashboard Slick 👌!
       </h1>
