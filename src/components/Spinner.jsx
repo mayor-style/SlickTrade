484425a14bee813953311c69d1567b585a6378b7
slick Outlaw <teslimshehu17@gmail.com>
2025-01-04 05:04:36 +0100

    implemented a spinner before confirmation summaery renders

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Spinner = ({ message=null, color, lg=false, xl=false}) => {
+const Spinner = ({ message=null, color, lg=false}) => {
   return (
     <div className="flex items-center justify-center text-center m-auto gap-2">
       <p className={`text-${color ? color : 'white'} max-sm:text-sm`}>{message}</p>
-      <span className={`animate-spin rounded-full border-2 border-gold border-t-transparent w-5 h-5  ${ lg? `w-14 h-14 border-[5px]`: ''}  ${xl? 'w-14 h-14 border-[8px]' : ''} `}></span>
+      <span className={`animate-spin rounded-full  border-gold border-t-transparent ${ lg? `w-14 h-14 border-[5px]`: 'border-[3px] w-5 h-5'}  `}></span>
     </div>
 
   );
